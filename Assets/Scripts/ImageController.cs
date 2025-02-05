a6756a01c6816f51b08c10dbd62197f7a2967de2
scureeee <1717souta@gmail.com>
2025-02-05 13:36:35 +0900

    create
    create

---
+++ Assets/Scripts/ImageController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static TurnController;
+
+public class ImageController : MonoBehaviour
+{
+    public Image targetImage; // 変更する Image コンポーネント
+    public Sprite playerSet;
+    public Sprite playerOpen;
+    public Sprite enemySet;
+    public Sprite enemyOpen;
+
+    private float delayTime = 1f; // 色を変えるまでの遅延時間（秒）
+
+    public GameObject explosion;
+
+    public GameObject safe;
+
+
+    public bool imageTrigger;
+
+    TurnController turnController;
+
+    void Start()
+    {
+        imageTrigger = true;
+        turnController = FindObjectOfType<TurnController>();
+    }
+
+    void Update()
+    {
+        // 現在のstateを取得
+        TurnController.PhaseState currentState = turnController.GetCurrentState();
+
+        if (currentState == PhaseState.PlayerChoiceToSetBomb && imageTrigger)
+        {
+            targetImage.sprite = playerSet;
+            targetImage.color = new Color(1f, 1f, 1f, 1f);
+
+            // 指定した時間後に透明度を変更
+            StartCoroutine(ChangeColorAfterDelay(delayTime));
+        }
+        else if(currentState == PhaseState.PlayerChoiceToOpenBox && imageTrigger)
+        {
+            targetImage.sprite = playerOpen;
+            targetImage.color = new Color(1f, 1f, 1f, 1f);
+
+            // 指定した時間後に透明度を変更
+            StartCoroutine(ChangeColorAfterDelay(delayTime));
+        }
+        else if(currentState == PhaseState.EnemyChoiceToSetBomb && imageTrigger)
+        {
+            targetImage.sprite = enemySet;
+            targetImage.color = new Color(1f, 1f, 1f, 1f);
+
+            // 指定した時間後に透明度を変更
+            StartCoroutine(ChangeColorAfterDelay(delayTime));
+        }
+        else if(currentState == PhaseState.EnemyChoiceToOpenBox && imageTrigger)
+        {
+            targetImage.sprite = enemyOpen;
+            targetImage.color = new Color(1f, 1f, 1f, 1f);
+
+            // 指定した時間後に透明度を変更
+            StartCoroutine(ChangeColorAfterDelay(delayTime));
+        }
+    }
+
+
+    // Coroutineを使って指定時間後に画像の色を変える
+    IEnumerator ChangeColorAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        imageTrigger = false;
+
+        // 2秒後に画像の透明度を変更
+        targetImage.color = new Color(1f, 1f, 1f, 0f); // 半透明に変更
+    }
+
+    public IEnumerator ExplosionSwitch()
+    {
+        explosion.SetActive(true);
+        yield return new WaitForSeconds(7f);
+        explosion.SetActive(false);
+    }
+
+    public void Safe()
+    {
+        StartCoroutine(SafeSwitch());
+    }
+    private IEnumerator SafeSwitch()
+    {
+        safe.SetActive(true);
+        yield return new WaitForSeconds(7f);
+        safe.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ResultJudge.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultJudge : MonoBehaviour
+{
+
+    private TurnController turnController;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        turnController = FindObjectOfType<TurnController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TurnController.playerLife == 0)
+        {
+            ResultText.resultNumber = 1;
+            Debug.Log("LifeÇ»ÇµîsñkÇ≈Ç∑");
+            SceneManager.LoadScene("ResultScene");
+        }
+
+        if (TurnController.enemyLife == 0)
+        {
+            ResultText.resultNumber = 2;
+            Debug.Log("LifeÇ»ÇµèüóòÇ≈Ç∑");
+            SceneManager.LoadScene("ResultScene");
+        }
+
+        if(TurnController.enemyPoint == OptionController.maxPoint)
+        {
+            ResultText.resultNumber = 3;
+            Debug.Log("Point You LOSE");
+            SceneManager.LoadScene("ResultScene");
+        }
+
+        if(TurnController.playerPoint == OptionController.maxPoint)
+        {
+            ResultText.resultNumber = 4;
+            Debug.Log("Point You WIN");
+            SceneManager.LoadScene("ResultScene");
+        }
+
+        if(turnController.objectArray.Length == 1)
+        {
+            if (TurnController.playerPoint < TurnController.enemyPoint)
+            {
+                ResultText.resultNumber = 5;
+                Debug.Log("Turn You LOSE");
+                SceneManager.LoadScene("ResultScene");
+            }
+
+            else if (TurnController.playerPoint > TurnController.enemyPoint)
+            {
+                ResultText.resultNumber = 6;
+                Debug.Log("Turn You WIN");
+                SceneManager.LoadScene("ResultScene");
+            }
+
+            else if (TurnController.playerPoint == TurnController.enemyPoint)
+            {
+                ResultText.resultNumber = 7;
+                Debug.Log("Draw");
+                SceneManager.LoadScene("ResultScene");
+            }
+        }
+
+        if (turnController.turnCount == OptionController.maxTurn)
+        {
+
+            if (TurnController.playerPoint < TurnController.enemyPoint)
+            {
+                ResultText.resultNumber = 5;
+                Debug.Log("Turn You LOSE");
+                SceneManager.LoadScene("ResultScene");
+            }
+
+            else if (TurnController.playerPoint > TurnController.enemyPoint)
+            {
+                ResultText.resultNumber = 6;
+                Debug.Log("Turn You WIN");
+                SceneManager.LoadScene("ResultScene");
+            }
+
+            else if (TurnController.playerPoint == TurnController.enemyPoint)
+            {
+                ResultText.resultNumber = 7;
+                Debug.Log("Draw");
+                SceneManager.LoadScene("ResultScene");
+
+            }
+        }
+
+    }
+} 
